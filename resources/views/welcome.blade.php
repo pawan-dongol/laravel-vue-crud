863962a398b7334b2f8e5ea9192dc09458a7b306
Pawan Dongol <pawandongol@gmail.com>
2018-09-21 07:46:38 +0545

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <script>window.Laravel = { csrfToken: '{{ csrf_token() }}'}</script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Larticles App</title>
+    <title>Lara-articles App</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
